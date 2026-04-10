eeb93a627d22714c5ff97c7fdcce8b4d8efff1ff
Oliver <122227371+Oliver4code@users.noreply.github.com>
2026-04-10 20:33:56 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,3 @@ set -e
 if ! zypper lr | grep -q "danklinux"; then
     sudo zypper addrepo -n danklinux https://download.opensuse.org/repositories/home:AvengeMedia:danklinux/16.0/home:AvengeMedia:danklinux.repo
 fi
-
-# DMS repo
-if ! zypper lr | grep -q "dmsrepo"; then
-    sudo zypper addrepo -n dmsrepo https://download.opensuse.org/repositories/home:/AvengeMedia:/dms/16.0/home:AvengeMedia:dms.repo
-fi
-
-sudo zypper refresh
-sudo zypper install -y dms
