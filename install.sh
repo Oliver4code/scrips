87c2a79acb7fc6e07dc0c0416e3a86b546a43678
Oliver <122227371+Oliver4code@users.noreply.github.com>
2026-04-10 20:39:43 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+set -e
 
+# Remove existing repos (ignore error if not exist)
+sudo zypper removerepo home_AvengeMedia_danklinux || true
+sudo zypper removerepo home_AvengeMedia_dms || true
 
+# Add repos again
+sudo zypper addrepo -f https://download.opensuse.org/repositories/home:AvengeMedia:danklinux/16.0/home:AvengeMedia:danklinux.repo
+sudo zypper addrepo -f https://download.opensuse.org/repositories/home:/AvengeMedia:/dms/16.0/home:AvengeMedia:dms.repo
+
+# Refresh and install
 sudo zypper refresh
 sudo zypper install -y dms
