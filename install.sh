fa5076100783ba7318507ca4a53de3da3eaf2f0e
Oliver <122227371+Oliver4code@users.noreply.github.com>
2026-04-10 20:27:25 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
-sudo zypper addrepo https://download.opensuse.org/repositories/home:AvengeMedia:danklinux/16.0/home:AvengeMedia:danklinux.repo
-sudo zypper addrepo https://download.opensuse.org/repositories/home:/AvengeMedia:/dms/16.0/home:AvengeMedia:dms.repo
+
+sudo zypper addrepo -f https://download.opensuse.org/repositories/home:AvengeMedia:danklinux/16.0/home:AvengeMedia:danklinux.repo
+sudo zypper addrepo -f https://download.opensuse.org/repositories/home:/AvengeMedia:/dms/16.0/home:AvengeMedia:dms.repo
+
 sudo zypper refresh
-sudo zypper install dms
+sudo zypper install -y dms
