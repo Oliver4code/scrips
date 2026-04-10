2ec60329fb7033e5150ac68d716c71850d78c827
Oliver <122227371+Oliver4code@users.noreply.github.com>
2026-04-10 20:29:55 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
-sudo zypper addrepo -f https://download.opensuse.org/repositories/home:AvengeMedia:danklinux/16.0/home:AvengeMedia:danklinux.repo
-sudo zypper addrepo -f https://download.opensuse.org/repositories/home:/AvengeMedia:/dms/16.0/home:AvengeMedia:dms.repo
+# Add DankLinux repo if not exists
+if ! zypper lr | grep -q "home:AvengeMedia:danklinux"; then
+    sudo zypper addrepo https://download.opensuse.org/repositories/home:AvengeMedia:danklinux/16.0/home:AvengeMedia:danklinux.repo
+fi
+
+# Add DMS repo if not exists
+if ! zypper lr | grep -q "home:AvengeMedia:dms"; then
+    sudo zypper addrepo https://download.opensuse.org/repositories/home:/AvengeMedia:/dms/16.0/home:AvengeMedia:dms.repo
+fi
 
 sudo zypper refresh
 sudo zypper install -y dms
