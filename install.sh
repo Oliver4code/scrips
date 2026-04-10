ce7e77bd2960a59439ec67085a63289c933d0bd2
Oliver <122227371+Oliver4code@users.noreply.github.com>
2026-04-10 20:34:56 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
-# DMS repo
-if ! zypper lr | grep -q "dmsrepo"; then
-    sudo zypper addrepo -n dmsrepo https://download.opensuse.org/repositories/home:/AvengeMedia:/dms/16.0/home:AvengeMedia:dms.repo
-fi
 
 sudo zypper refresh
 sudo zypper install -y dms
